6bdf7d7d204f6e2fa93f809c179caa3625beeb89
AzureLuo-CN <1850049920@qq.com>
2020-09-01 18:14:15 +0800

    Initial commit

---
+++ app/src/main/java/com/example/njupt_mylife/ui/picture/PictureViewModel.java
@@ -0,0 +1,19 @@
+package com.example.njupt_mylife.ui.picture;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class PictureViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public PictureViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is picture fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
